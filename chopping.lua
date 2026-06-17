2724cf320a7af97439b5878042ed5c3159461810
Alisa Sireneva <me@purplesyringa.moe>
2026-06-18 01:38:00 +0300

    Fix line number tracking in chopping

--- chopping.lua
+++ chopping.lua
@@ -3,17 +3,34 @@ local function makeChopper(code)
     local last_added = 0
 
     local chopper = {}
+
     function chopper.cut(first, last, replacement)
+        last = math.max(last, first - 1)
+        replacement = replacement or ""
+
         assert(first > last_added, "cutting an already cut part")
         table.insert(parts, code:sub(last_added + 1, first - 1))
-        if replacement then
+
+        local newlines_to_add = 0
+        for _ in code:sub(first, last):gmatch("\n") do
+            newlines_to_add = newlines_to_add + 1
+        end
+        for _ in replacement:gmatch("\n") do
+            newlines_to_add = newlines_to_add - 1
+        end
+        assert(newlines_to_add >= 0, "more newlines removed than added")
+        replacement = replacement .. ("\n"):rep(newlines_to_add)
+        if replacement ~= "" then
             table.insert(parts, replacement)
         end
+
         last_added = last
     end
+
     function chopper.insert(pos, value)
         chopper.cut(pos, pos - 1, value)
     end
+
     function chopper.finish()
         table.insert(parts, code:sub(last_added + 1))
         return table.concat(parts)
